525aca6b0284da44e291d17db413d89e4068e6ea
Yui9611 <white.plum.q@gmail.com>
2024-05-31 00:31:10 +0800

    login

--- Login/src/App.vue
+++ Login/src/App.vue
@@ -7,20 +7,20 @@ import LogIn from "./components/LogIn.vue";
     <div class="bg-White flex pb-8 px-5 rounded-xl">
       <div class="justify-items-center">
         <img class="w-56 h-40 m-10" src="\src\assets\lit_logo.png" />
-        <p class="font-semibold text-Blue text-3xl">
+        <p class="font-semibold text-Blue text-3xl m-10">
           復興高中<br />重補修報名系統
         </p>
+        <LogIn />
       </div>
-      <div class="pt-20">
-        <input
+      <div>
+        <!-- <input
           class="flex w-240 h-12 m-5 p-2 border-2 rounded-2xl"
           placeholder="請輸入學號..."
         />
         <input
           class="flex w-240 h-12 m-5 p-2 border-2 rounded-2xl"
           placeholder="請輸入身份證號碼"
-        />
-        <LogIn />
+        /> -->
       </div>
     </div>
   </div>
--- Login/src/components/LogIn.vue
+++ Login/src/components/LogIn.vue
@@ -1,25 +1,12 @@
-<script setup>
-const resp = await fetch(url, get_user_fetch_options);
-const google_login_callback = (response) => {
-  const token = response.credential;
-  const result = login_with_google_id_token(token);
-  if (result.status !== 200) {
-    error_data.status = result.status;
-    error_data.desc = result.desc;
-    states.error_occur = true;
-    return;
-  }
-  // console.log("Handle the response", response);
-};
-</script>
+<script setup></script>
 
 <template>
-  <button
+  <!-- <button
     class="bg-Blue mt-5 p-2 px-3 rounded-xl text-White font-semibold"
     href="Registration.html"
   >
     登入
-  </button>
+  </button> -->
   <GoogleLogin :callback="google_login_callback" />
 </template>
 
