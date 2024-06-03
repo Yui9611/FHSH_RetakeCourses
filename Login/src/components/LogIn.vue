279b9b1c66df72e0dddffff18f643268dfe45792
Yui9611 <white.plum.q@gmail.com>
2024-06-03 08:49:02 +0800

    add

--- Login/src/components/LogIn.vue
+++ Login/src/components/LogIn.vue
@@ -1,12 +1,25 @@
-<script setup></script>
+<script setup>
+const resp = await fetch(url, get_user_fetch_options);
+const google_login_callback = (response) => {
+  const token = response.credential;
+  const result = await resp.login_with_google_id_token(token);
+  if (result.status !== 200) {
+    error_data.status = result.status;
+    error_data.desc = result.desc;
+    states.error_occur = true;
+    return;
+  }
+  // console.log("Handle the response", response);
+};
+</script>
 
 <template>
-  <!-- <button
+  <button
     class="bg-Blue mt-5 p-2 px-3 rounded-xl text-White font-semibold"
     href="Registration.html"
   >
     登入
-  </button> -->
+  </button>
   <GoogleLogin :callback="google_login_callback" />
 </template>
 
